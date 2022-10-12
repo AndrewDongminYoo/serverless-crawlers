bcecda066d098c2cf004ca835d1c500664189ef6
Dongmin,Yoo <ydm2790@gmail.com>
2022-10-12 21:22:47 +0900

    🔥modify columns name

--- crawler/api.py
+++ crawler/api.py
@@ -18,16 +18,16 @@ CIRCLE_URL = 'https://circlechart.kr'
 
 def fetch_chart_api(period: str, chart_type: str, dt: datetime):
     queries = {'termGbn': period}
-    ymd = date_to_string(dt, period)
+    yyyymm = date_to_string(dt, period)
     if chart_type == "global":
-        queries['yyyymmdd'] = ymd
+        queries['yyyymmdd'] = yyyymm
     elif chart_type == "album":
         queries['hitYear'] = date_to_string(dt, 'year')
         queries['targetTime'] = date_to_string(dt, 'M')
         queries['nationGbn'] = 'T'
         queries['yearTime'] = '3'
         queries['curUrl'] = f'{CIRCLE_URL}/page_chart/{chart_type}.circle{set_queries(queries)}'
-    url = f'{CIRCLE_URL}/data/api/chart/{chart_type}'
+    url = f'{CIRCLE_URL}/data/api/chart/{chart_type}{set_queries(queries)}'
     headers = {
         'Accept': 'application/json, text/javascript, */*; q=0.01',
         'Connection': 'keep-alive',
@@ -35,10 +35,10 @@ def fetch_chart_api(period: str, chart_type: str, dt: datetime):
         'X-Requested-With': 'XMLHttpRequest',
         'Referer': f'{CIRCLE_URL}/page_chart/{chart_type}.circle'
     }
-    req = post(url, headers=headers, params=queries)
+    req = post(url, headers=headers)
     result = json.loads(req.text)
     if result["ResultStatus"] == "OK":
-        return as_chart_array(object_to_list(result["List"]), ymd)
+        return as_chart_array(object_to_list(result["List"]), yyyymm, url)
     else:
         return {
             "status_code": req.status_code,
--- crawler/comon.py
+++ crawler/comon.py
@@ -1,63 +1,66 @@
 class GlobalChart(object):
     month = ''
-    album_image = ''
-    album_name = ''
-    artist_name = ''
-    distribute_company = ''
-    producing_company = ''
-    rank = "101"
+    link = ''
+    image = ''
+    album = ''
+    artist = ''
+    distributor = ''
+    producer = ''
+    ranking = "101"
     rank_status = '-'
-    # total_counts = 0
-    song_title = ''
+    # sales_volume = "0"
+    title = ''
 
-    def __init__(self, data: dict):
-        self.month = data['month']
-        self.album_image = "https://circlechart.kr/uploadDir/" + data['ALBUMIMG']
-        self.album_name = data['Album']
-        self.artist_name = data['Artist']
-        self.distribute_company = data['CompanyDist']
-        self.producing_company = set_producer(data['CompanyMake'], data['Album'])
-        self.rank = data['Rank']  # 랭킹
+    def __init__(self, data: dict, month, url):
+        self.month = month[2:]
+        self.link = url
+        self.image = "https://circlechart.kr/uploadDir/" + data['ALBUMIMG']
+        self.album = data['Album']
+        self.artist = data['Artist']
+        self.distributor = data['CompanyDist']
+        self.producer = set_producer(data['CompanyMake'], data['Album'])
+        self.ranking = data['Rank']  # 랭킹
         self.rank_status = data['RankStatus']  # 전월대비 (+1)
-        # self.total_counts = data[""]
-        self.song_title = data['Title']  # 곡명
+        # self.sales_volume = data[""]
+        self.title = data['Title']  # 곡명
 
 
 class AlbumChart(object):
     month = ''
-    album_image = ''
-    album_name = ''
-    artist_name = ''
-    distribute_company = ''
-    producing_company = ''
-    rank = "101"
+    link = ''
+    image = ''
+    album = ''
+    artist = ''
+    distributor = ''
+    producer = ''
+    ranking = "101"
     rank_status = '-'
-    total_counts = 0
-    # song_title = ''
+    sales_volume = "0 / 0"
+    title = ''
 
-    def __init__(self, data: dict):
-        self.month = data['month']
-        self.album_image = "https://circlechart.kr" + data['FILE_NAME']
-        self.album_name = data['ALBUM_NAME']
-        self.artist_name = data['ARTIST_NAME']
-        self.distribute_company = data['de_nm']
-        self.producing_company = get_producer(data['ALBUM_NAME'])
-        self.rank = data['SERVICE_RANKING']  # 랭킹
+    def __init__(self, data: dict, month, url):
+        self.month = month[2:]
+        self.link = url
+        self.image = "https://circlechart.kr" + data['FILE_NAME']
+        self.album = data['ALBUM_NAME']
+        self.artist = data['ARTIST_NAME']
+        self.distributor = data['de_nm']
+        self.producer = get_producer(data['ALBUM_NAME'])
+        self.ranking = data['SERVICE_RANKING']  # 랭킹
         self.rank_status = rank_status(data['RankStatus'], data['RankChange'])  # 전월대비 (1up)
-        self.total_counts = data["Total_CNT"]  # 앨범 판매량
-        # self.song_title = data['']
+        self.sales_volume = data["Album_CNT"] + " / " + data["Total_CNT"]  # 앨범 판매량
+        # self.title = data['']
 
 
-def as_chart(data: dict, ts: str):
-    data['month'] = ts
+def as_chart(data: dict, ts: str, url: str):
     if "Total_CNT" in data.keys():
-        return AlbumChart(data)
+        return AlbumChart(data, ts, url)
     elif "Title" in data.keys():
-        return GlobalChart(data)
+        return GlobalChart(data, ts, url)
 
 
-def as_chart_array(array: list, ts: str):
-    return [as_chart(x, ts).__dict__ for x in array]
+def as_chart_array(array: list, ts: str, url: str):
+    return [as_chart(x, ts, url).__dict__ for x in array]
 
 
 def rank_status(status: str, change: str):
